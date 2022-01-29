005f088bf69e9630aad653663c6ab8dcc0fa0d28
nicolmendoza <nicolmendozamattos@gmail.com>
2022-01-29 12:26:43 -0500

    creando el admi

--- src/models/orders.js
+++ src/models/orders.js
@@ -1,26 +1,35 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const orderSchema = new Schema({
-  userId: {
-    type: String,
-    required: true,
-  },
-  client: {
-    type: String,
-    required: true,
-  },
-  products: [{
-    qty: {
-      type: Number,
-    },
-    productId: {
+const orderSchema = new Schema(
+  {
+    userId: {
       type: String,
+      required: true,
     },
-  }],
+    client: {
+      type: String,
+      required: true,
+    },
+    products: [
+      {
+        qty: {
+          type: Number,
+        },
+        productId: {
+          type: String,
+          required:true
+        },
+      },
+    ],
+    status: {
+      type: String,
+      default:'pending'
+    },
+  },
+  {
+    timestamps: {createdAt:'dataEntry', updatedAt:'dateProcessed' },
+    versionKey: false,
+  }
+);
 
-}, {
-  timestamps: true,
-  versionKey: false,
-});
-
-module.exports = model('Order', orderSchema);
+module.exports = model("Order", orderSchema);
---
+++ src/models/users.js
@@ -0,0 +1,46 @@
+
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcryptjs");
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'El nombre es obligatorio'],
+      unique: true,
+      index: true, // acelera busqueda
+    },
+    password: {
+      type: String,
+      required: [true, 'La contraseña es obligatoria'],
+    },
+    roles: {
+      admin: {
+        type: Boolean,
+      },
+    },
+  }, {
+    timestamps: true,
+    versionKey: false,
+  });
+
+userSchema.methods.encryptPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hash(password, salt);
+};
+
+module.exports = model("User", userSchema);
+
+
+
+
+
+
+//voy a crear un método para encriptar la contraseña
+//la funcion recibe una contraseña que va a ser encriptada
+//bcrypt se encarga de cifrar la contraseña
+//el metodo gensalt , método que hace más seguro, cuantas veces queermos aplicar el algotitmo
+//el metodo hash convierte el string a unos caracteres indecifrables, dificiles de entender
+
+//el método "REF" sirve para decir que tiene una referencia, o está relacionado con otro modelo de datos
+//el tipo de dato que estoy guradando es un OjectId
+//me sirve para relacionar el esquema de ROL con el USERS
