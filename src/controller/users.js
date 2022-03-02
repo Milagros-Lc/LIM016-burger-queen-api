b26d3b9065fa4b240d92f3a7eed79ac92895662d
Milagros LC <evidal0710@ich.edu.pe>
2022-03-02 11:08:17 -0500

    update users.js controller

--- src/controller/users.js
+++ src/controller/users.js
@@ -5,20 +5,20 @@ const mongoose = require("mongoose");
 const res = require("express/lib/response");
 
 const {
-  pagination,
-  isValidPassword,
-  isValidEmail,
+    pagination,
+    isValidPassword,
+    isValidEmail,
 } = require("../utils/utils.js");
 const { isAdmin } = require("../middleware/auth");
 const ObjectId = require("mongoose").Types.ObjectId;
 
-const getUserByIdOrEmail = async (uid) => {
-  if (!ObjectId.isValid(uid)) {
-    const user = await User.findOne({ email: `${uid}` });
+const getUserByIdOrEmail = async(uid) => {
+    if (!ObjectId.isValid(uid)) {
+        const user = await User.findOne({ email: `${uid}` });
+        return user;
+    }
+    const user = await User.findOne({ _id: `${uid}` });
     return user;
-  }
-  const user = await User.findOne({ _id: `${uid}` });
-  return user;
 };
 
 //El método test() ejecuta la búsqueda de una ocurrencia entre una expresión regular y una cadena especificada. Devuelve true o false.
@@ -28,165 +28,163 @@ const getUserByIdOrEmail = async (uid) => {
 // };
 
 module.exports = {
-  getUsers: async (req, resp, next) => {
-    try {
-      const limit = parseInt(req.query.limit) || 10;
-      const page = parseInt(req.query.page) || 1;
-      const users = await User.paginate({}, { limit, page });
+    getUsers: async(req, resp, next) => {
+        try {
+            const limit = parseInt(req.query.limit) || 10;
+            const page = parseInt(req.query.page) || 1;
+            const users = await User.paginate({}, { limit, page });
 
-      const url = `${req.protocol}://${req.get("host") + req.path}`;
+            const url = `${req.protocol}://${req.get("host") + req.path}`;
 
-      const links = pagination(users, url, page, limit, users.totalPages);
-      resp.links(links);
-      resp.status(200).json(users.docs);
-    } catch (error) {
-      next(error);
-    }
-  },
+            const links = pagination(users, url, page, limit, users.totalPages);
+            resp.links(links);
+            resp.status(200).json(users.docs);
+        } catch (error) {
+            next(error);
+        }
+    },
 
-  createAdmi: async (adminEmail, adminPassword) => {
-    const adminUser = {
-      email: adminEmail,
-      password: await User.encryptPassword(adminPassword),
-      roles: { admin: true },
-    };
-    const user = new User(adminUser);
-    console.info("admi creado");
-    return await user.save();
-  },
+    createAdmi: async(adminEmail, adminPassword) => {
+        const adminUser = {
+            nameUser: 'Maria',
+            email: adminEmail,
+            password: await User.encryptPassword(adminPassword),
+            roles: { admin: true },
+            ////////////
+            image: '',
+        };
+        const user = new User(adminUser);
+        console.info("admi creado");
+        return await user.save();
+    },
 
-  getUserById: async (req, resp, next) => {
-    const { uid } = req.params;
-    const user = await getUserByIdOrEmail(uid);
+    getUserById: async(req, resp, next) => {
+        const { uid } = req.params;
+        const user = await getUserByIdOrEmail(uid);
 
-    if (!user) return next(404);
+        if (!user) return next(404);
 
-    // {403} si no es ni admin o la misma usuaria
+        // {403} si no es ni admin o la misma usuaria
+        if (isAdmin(req) || req.authToken.uid === user._id.toString()) {
+            return resp.status(200).json(user);
+        }
 
-    if (isAdmin(req) || req.authToken.uid === user._id.toString()) {
-      return resp.status(200).json(user);
-    }
+        return resp.status(403).json({ message: "no tienes permisos" });
+    },
 
-    return resp.status(403).json({ message: "no tienes permisos" });
-  },
+    postUser: async(req, resp, next) => {
+        try {
+            const { email, password, roles, nameUser, image } = req.body;
+            if (!email || !password)
+                return resp.status(400).json({ message: "no ingresó  email o password" });
 
-  postUser: async (req, resp, next) => {
-    try {
-      const { email, password, roles, nameUser,image } = req.body;
-      if (!email || !password)
-        return resp.status(400).json({ message: "no ingresó  email o password" });
+            if (!isValidEmail(email) || !isValidPassword(password))
+                return resp.status(400).json({
+                    message: "el formato de la conraaseña o email no es correcto",
+                });
 
-      if (!isValidEmail(email) || !isValidPassword(password))
-        return resp.status(400).json({
-          message: "el formato de la conraaseña o email no es correcto",
-        });
+            const userFind = await User.findOne({ email: email });
 
-      const userFind = await User.findOne({ email: email });
+            if (userFind)
+                return resp
+                    .status(403)
+                    .json({ message: "ya existe un usuario con ese email" });
 
-      if (userFind)
-        return resp
-          .status(403)
-          .json({ message: "ya existe un usuario con ese email" });
+            let newUser = new User({
+                email,
+                roles,
+                password: await User.encryptPassword(password),
+                nameUser,
+                image
+            });
 
-      let newUser = new User({
-        email,
-        roles,
-        password: await User.encryptPassword(password),
-        nameUser,
-        image
-      });
+            await newUser.save();
+            newUser = await User.findOne({ email: email }).select("-password");
+            return resp.status(200).json(newUser);
+        } catch (e) {
+            next(e)
+        }
+    },
 
-      await newUser.save();
-      newUser = await User.findOne({ email: email }).select("-password");
-      return resp.status(200).json(newUser);
-    } catch (e) {
-      next(e)
-    }
-  },
+    updateUser: async(req, resp, next) => {
+        try {
+            const uid = req.params.uid;
+            const admin = await isAdmin(req);
+            // console.log(uid)
+            // console.log(req.params.uid)
+            // console.log(ObjectId.isValid(uid))
+            // console.log(admin)
 
-  updateUser: async (req, resp, next) => {
-    try {
-      const uid = req.params.uid;
-      const admin = await isAdmin(req);
-      // console.log(uid)
-      // console.log(req.params.uid)
-      // console.log(ObjectId.isValid(uid))
-      // console.log(admin)
+            // resp.status(200).json(user);
+            if (!uid)
+                return resp.status(400).json({ message: "no provee de un id o email" });
 
-      // resp.status(200).json(user);
-      if (!uid)
-        return resp.status(400).json({ message: "no provee de un id o email" });
+            const user = await getUserByIdOrEmail(uid);
+            //if(!ObjectId.isValid(uid)) return resp.status(404).json({message:'This product does not exist, please check the Id'})
+            if (!user)
+                return resp
+                    .status(404)
+                    .json({ message: "el usuario solicitado no existe" });
 
-      const user = await getUserByIdOrEmail(uid);
-      //if(!ObjectId.isValid(uid)) return resp.status(404).json({message:'This product does not exist, please check the Id'})
-      if (!user)
-        return resp
-          .status(404)
-          .json({ message: "el usuario solicitado no existe" });
+            if (req.authToken.uid !== user._id.toString() && !admin) {
+                return resp
+                    .status(403)
+                    .json({ message: "es necesario ser admin o el dueño del usuario" });
+            }
 
-      if (req.authToken.uid !== user._id.toString() && !admin) {
-        return resp
-          .status(403)
-          .json({ message: "es necesario ser admin o el dueño del usuario" });
-      }
+            let { email, password, roles, nameUser, image } = req.body;
 
-      let { email, password, roles , nameUser,image} = req.body;
-      
-      if (roles && !admin)
-        return resp
-          .status(403)
-          .json({ message: "no tienes permisos para realizar esta operación" });
+            if (roles && !admin)
+                return resp
+                    .status(403)
+                    .json({ message: "no tienes permisos para realizar esta operación" });
 
-      if (Object.entries(req.body).length === 0)
-        return resp
-          .status(400)
-          .json({ message: "Faltan datos para actualizar" });
+            if (Object.entries(req.body).length === 0)
+                return resp
+                    .status(400)
+                    .json({ message: "Faltan datos para actualizar" });
 
-      if (!password) {
-         password = user.password;
-      }else{
-        password =  bcrypt.hashSync(password, 10)
-      }
-     
-      if (!email) email = user.email;
-      if (!roles) roles = user.roles;
-      if (!image) image = user.image;
-      if (!nameUser) nameUser = user.nameUser;
-      //const value = ObjectId.isValid(uid) ? { _id: uid } : { email: uid };
-      if (!isValidEmail(email) || !isValidPassword(password))
-        return resp.status(400).json({
-          message: "el formato de la contraseña o email no es correcto",
-        });
-      const userUpdate = await User.findByIdAndUpdate(
-        { _id: `${user._id}` },
-        { email, password, roles , image,nameUser},
-        { new: true, useFindAndModify: false }
-      );
-      return resp.status(200).json(userUpdate);
-    } catch (err) {
-      next(err);
-    }
-  },
+            if (!password) {
+                password = user.password;
+            } else {
+                password = bcrypt.hashSync(password, 10)
+            }
 
-  deleteUser: async (req, resp, next) => {
-    try {
-      const { uid } = req.params;
+            if (!email) email = user.email;
+            if (!roles) roles = user.roles;
+            if (!image) image = user.image;
+            if (!nameUser) nameUser = user.nameUser;
+            //const value = ObjectId.isValid(uid) ? { _id: uid } : { email: uid };
+            if (!isValidEmail(email) || !isValidPassword(password))
+                return resp.status(400).json({
+                    message: "el formato de la contraseña o email no es correcto",
+                });
+            const userUpdate = await User.findByIdAndUpdate({ _id: `${user._id}` }, { email, password, roles, image, nameUser }, { new: true, useFindAndModify: false });
+            return resp.status(200).json(userUpdate);
+        } catch (err) {
+            next(err);
+        }
+    },
 
-      const user = await getUserByIdOrEmail(uid);
+    deleteUser: async(req, resp, next) => {
+        try {
+            const { uid } = req.params;
 
-      if (!user)
-        return resp.status(404).json({ message: "el usuario no existe" });
+            const user = await getUserByIdOrEmail(uid);
 
-      if (req.authToken.uid === user._id.toString() || isAdmin(req)) {
-        const id = user._id.toString();
-        const userd = await User.findOneAndDelete({ _id: `${id}` })
-        return resp.status(200).json(userd);
-      }
-      return resp.status(403).json({ message: "no tiene permisos para eliminar al usuario" });
-    } catch(e) {
-      next(e)
-    }
-  },
+            if (!user)
+                return resp.status(404).json({ message: "el usuario no existe" });
+
+            if (req.authToken.uid === user._id.toString() || isAdmin(req)) {
+                const id = user._id.toString();
+                const userd = await User.findOneAndDelete({ _id: `${id}` })
+                return resp.status(200).json(userd);
+            }
+            return resp.status(403).json({ message: "no tiene permisos para eliminar al usuario" });
+        } catch (e) {
+            next(e)
+        }
+    },
 };
 
 //isValidObjectId()se usa más comúnmente para probar un ID de objeto esperado
