fbec03118f3748f0eb471e3b8491887c6700005e
Milagros LC <evidal0710@ich.edu.pe>
2022-03-02 19:24:12 -0500

    update 4

--- src/models/products.js
+++ src/models/products.js
@@ -1,32 +1,49 @@
 const { Schema, model } = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate-v2')
 
+/*
+"id": 1,
+"name": "Café americano",
+"price": 8,
+"image": "../../assets/images/cafe_americano.png",
+"type": "cafes",
+"dateEntry": "21/01/2022 09:24:00",
+"qty": 0,
+"subTotal": 8,
+"messageCard": "Producto agregado."
+*/
 const productSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  image: {
-    type: String, 
-  },
-  type: {
-    type: String, 
-  },
-  dateEntry: {
-    type: Date,
-    default: Date.now,
-  },
-  comment:{
-    type:"String",
-    required:false
-  }
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    image: {
+        type: String,
+    },
+    type: {
+        type: String,
+    },
+    dateEntry: {
+        type: Date,
+        default: Date.now,
+    },
+    qty: {
+        type: Number,
+    },
+    subTotal: {
+        type: Number,
+    },
+    messageCard: {
+        type: String,
+        required: false
+    }
 }, {
-  timestamps: true,
-  versionKey:false,
+    timestamps: true,
+    versionKey: false,
 });
 
 productSchema.plugin(mongoosePaginate)
