8b4c504ff7582e04d07ae69ca1734e4ed582d80e
Milagros LC <evidal0710@ich.edu.pe>
2022-03-03 10:06:10 -0500

    update order

--- src/controller/orders.js
+++ src/controller/orders.js
@@ -2,105 +2,103 @@ const { isValidObjectId } = require("mongoose");
 const Order = require("../models/orders.js");
 const { pagination } = require("../utils/utils");
 module.exports = {
-  getOrders: async (req, resp, next) => {
-    try {
-      const limit = parseInt(req.query.limit, 10) || 10;
-      const page = parseInt(req.query.page, 10) || 1;
-      const populate = 'products.product'
+    getOrders: async(req, resp, next) => {
+        try {
+            const limit = parseInt(req.query.limit, 10) || 10;
+            const page = parseInt(req.query.page, 10) || 1;
+            const populate = 'products.product'
 
-      const orders =  await Order.paginate({}, {limit, page, populate})
-      
-      if(orders){
-          const URL = `${req.protocol}://${req.headers.host + req.path}`;
-            
-          const link = pagination(orders, URL, page, limit, orders.totalPages )
+            const orders = await Order.paginate({}, { limit, page, populate })
 
-          resp.links(link);
-          return resp.status(200).json((orders.docs))
-      }
-      return resp.json('Database has not orders');
-    } catch (e) {
-      next(e)
-    }
-  },
-  getOrderById: async (req, resp, next) => {
-    try {
-      const { orderId } = req.params;
-      if (!isValidObjectId(orderId)) return resp.status(404).json({ message: "no es válido el orderId" });
-      const order = await Order.findById({ _id: req.params.orderId }).populate('products.product');
-      if (!order) return resp.status(404).json({ message: "la orden no existe" });
-      return resp.status(200).json(order);
-    } catch(e) {
-      next(e)
-    }
-  },
-  postOrder: async (req, res, next) => {
-      try {const { userId, client, products } = req.body;
-      if (!products || products.length === 0) return next(400);
-      const newOrder = new Order({
-        userId,
-        client,
-        products: products.map((product) => ({
-          qty: product.qty,
-          product: product.productId,
-          comment:product.comment
-        })),
+            if (orders) {
+                const URL = `${req.protocol}://${req.headers.host + req.path}`;
 
-      });
-      const order = await newOrder.save();
-      const orderUpdate = await Order.findOne({ _id: order._id }).populate(
-        "products.product"
-      );
-      return res.status(200).json(orderUpdate);
-      } catch (e) {
-        next(e)
-      }
-  },
-  updateOrder: async (req, resp, next) => {
-    try {
-    const { orderId } = req.params;
+                const link = pagination(orders, URL, page, limit, orders.totalPages)
 
-    if (!isValidObjectId(orderId)) return resp.status(404).json({message:"no es válido es orderId"});
-    const statusOrder = [
-      "pending",
-      "canceled",
-      "delivering",
-      "delivered",
-      "preparing",
-    ];
-    if (Object.keys(req.body).length === 0)
-      return resp
-        .status(400)
-        .json({ message: "no se ha indicado datos a modificar" });
-    if (!statusOrder.includes(req.body.status)) return next(400);
+                resp.links(link);
+                return resp.status(200).json((orders.docs))
+            }
+            return resp.json('Database has not orders');
+        } catch (e) {
+            next(e)
+        }
+    },
+    getOrderById: async(req, resp, next) => {
+        try {
+            const { orderId } = req.params;
+            if (!isValidObjectId(orderId)) return resp.status(404).json({ message: "no es válido el orderId" });
+            const order = await Order.findById({ _id: req.params.orderId }).populate('products.product');
+            if (!order) return resp.status(404).json({ message: "la orden no existe" });
+            return resp.status(200).json(order);
+        } catch (e) {
+            next(e)
+        }
+    },
+    postOrder: async(req, res, next) => {
+        try {
+            const { userId, client, products } = req.body;
+            if (!products || products.length === 0) return next(400);
+            const newOrder = new Order({
+                userId,
+                client,
+                products: products.map((product) => ({
+                    //qty: product.qty,
+                    product: product.productId,
+                    //comment:product.comment
+                })),
 
-    const orderUpdated = await Order.findByIdAndUpdate(
-      { _id: orderId },
-      { $set: req.body },
-      { new: true, useFindAndModify: false }
-    );
-    return resp.status(200).json(orderUpdated);
-    } catch(e) {
-      next(e)
-    }
-  },
-  deleteOrder: async (req, resp, next) => {
-    try {
-      const { orderId } = req.params;
-      if (!isValidObjectId(orderId))
-        return resp.status(404).json({ message: "no ha indicado el orderId" });
 
-      if (!orderId)
-        return resp.status(404).json({ message: "no ha indicado el orderId" });
-      const res = await Order.findOne({ _id: req.params.orderId });
-      if (!res)
-        return resp.status(404).json({ message: "el producto no existe" });
-      await Order.findByIdAndDelete({ _id: req.params.orderId });
-      return resp.status(200).json({ message: "eliminado " });
-    } catch (e) {
-      next(e)
-    }
-  },
+            });
+            const order = await newOrder.save();
+            const orderUpdate = await Order.findOne({ _id: order._id }).populate(
+                "products.product"
+            );
+            return res.status(200).json(orderUpdate);
+        } catch (e) {
+            next(e)
+        }
+    },
+    updateOrder: async(req, resp, next) => {
+        try {
+            const { orderId } = req.params;
+
+            if (!isValidObjectId(orderId)) return resp.status(404).json({ message: "no es válido es orderId" });
+            const statusOrder = [
+                "pending",
+                "canceled",
+                "delivering",
+                "delivered",
+                "preparing",
+            ];
+            if (Object.keys(req.body).length === 0)
+                return resp
+                    .status(400)
+                    .json({ message: "no se ha indicado datos a modificar" });
+            if (!statusOrder.includes(req.body.status)) return next(400);
+
+            const orderUpdated = await Order.findByIdAndUpdate({ _id: orderId }, { $set: req.body }, { new: true, useFindAndModify: false });
+            return resp.status(200).json(orderUpdated);
+        } catch (e) {
+            next(e)
+        }
+    },
+    deleteOrder: async(req, resp, next) => {
+        try {
+            const { orderId } = req.params;
+            if (!isValidObjectId(orderId))
+                return resp.status(404).json({ message: "no ha indicado el orderId" });
+
+            if (!orderId)
+                return resp.status(404).json({ message: "no ha indicado el orderId" });
+            const res = await Order.findOne({ _id: req.params.orderId });
+            if (!res)
+                return resp.status(404).json({ message: "el producto no existe" });
+            await Order.findByIdAndDelete({ _id: req.params.orderId });
+            return resp.status(200).json({ message: "eliminado " });
+        } catch (e) {
+            next(e)
+        }
+    },
 };
 
 // convertir un string a un número -----parseInt
--- src/models/orders.js
+++ src/models/orders.js
@@ -2,50 +2,84 @@ const { Schema, model } = require("mongoose");
 const mongoosePaginate = require("mongoose-paginate-v2");
 
 
-const orderSchema = new Schema(
-  {
-    userId: {
-      type: String,
-      required: true,
-    },
-    client: {
-      type: String,
-      
-    },
-    products: [{
-      qty: {
-        type: Number,
-        default: 1,
-      },
-      product: {
-        type: Schema.Types.ObjectId,
-        ref: 'Product',
-      },
-      comment:{
-        type:String,
-        required:false
-      }
-    }],
-    status: {
-      type: String,
-      default: "pending",
+const orderSchema = new Schema({
+        userId: {
+            type: String,
+            required: true,
+        },
+        client: {
+            type: String,
+        },
+        products: [{
+            /* qty: {
+              type: Number,
+              default: 0,
+            }, */
+            product: {
+                type: Schema.Types.ObjectId,
+                ref: 'Product',
+            },
+            /*  comment:{
+               type:String,
+               required:false
+             } */
+        }],
+        status: {
+            type: String,
+            default: "pending",
 
-    },
-    dateEntry: {
-      type: Date,
-      default: Date.now,
-    },
-    dateProcessed: {
-      type: Date,
-      required: false,
-      default: Date.now,
-    },
-  },
-  {
-    timestamps: true,
-    versionKey: false,
-  }
-  
+        },
+        dateEntry: {
+            type: Date,
+            default: Date.now,
+        },
+        dateProcessed: {
+            type: Date,
+            required: false,
+            default: Date.now,
+        },
+        dateDelivering: {
+            type: Date,
+            required: false,
+            default: Date.now,
+        },
+        dateDone: {
+            type: Date,
+            required: false,
+            default: Date.now,
+        },
+        dateCanceled: {
+            type: Date,
+            required: false,
+            default: Date.now,
+        },
+        timeResult: {
+            type: String,
+            default: "",
+
+        },
+        additional: {
+            type: String,
+            default: "",
+
+        },
+        totalQty: {
+            type: Number,
+            required: false,
+        },
+        total: {
+            type: Number,
+            required: false,
+        },
+        numberTable: {
+            type: Number,
+            required: false,
+        },
+    }, {
+        timestamps: true,
+        versionKey: false,
+    }
+
 );
 
 orderSchema.plugin(mongoosePaginate);
